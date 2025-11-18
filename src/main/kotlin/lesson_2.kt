641b77c56d07cf9dc9e4a323aa4e3be2f758b55a
eliteBarbarian7 <alfimovdaniil2006@gmail.com>
2025-11-18 18:14:36 +0700

    second save

--- src/main/kotlin/lesson_2.kt
+++ src/main/kotlin/lesson_2.kt
@@ -2,4 +2,16 @@ package org.example
 
 fun main() {
 
+    val chislo1 = 5
+
+    var chislo2 = readln().toInt()
+
+    println(chislo1 + chislo2)
+
+
+
+    chislo2 = readln().toInt()
+
+    println(chislo2/       chislo1)
+
 }
--- src/main/kotlin/lesson_3.kt
+++ src/main/kotlin/lesson_3.kt
@@ -1,5 +1,15 @@
 package org.example
 
 fun main() {
+    // конкатенация строк
+    val zdarova = "privet"
+    val space = " "
 
+    val chislo = readln().toInt()
+
+    println("What is your main question?" + zdarova + space)
+
+
+    // интерполяция строк
+    println("What is $zdarova your main question? \'${5 + chislo}\'")
 }
